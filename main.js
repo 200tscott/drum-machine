56881ad1c3e6660f21930c19f2bb2489fe8b5f76
200tscott <terrell.scott@codeimmersives.com>
2022-04-03 14:44:21 -0400

    first

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+const hi = new Audio('sounds/hi-hat.mp3');
+const kick = new Audio('sounds/kick-drum.mp3');
+const snare = new Audio('sounds/snare-drum.mp3');
 
+let Metronome = document.querySelector("#metronome")
 // This function is called every 600ms
-function update() {
+let count = 1
 
-    // Play the 'tick' sound
-    tick.play();
+function update() { 
+count ++
+if (Metronome.checked === true)
+if (count % 4 === 0){
+    tock.play()
+}else{
+    tick.play()
+}
+
+
 }
 
 // This function sets up update() to be called every 600ms
